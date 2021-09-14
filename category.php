57c963df14e5a5ad6f65098ce491801e943b178a
dufva <sussiedufva@hotmail.com>
2021-09-14 12:34:45 +0200

    category.php added

---
+++ category.php
@@ -0,0 +1,56 @@
+<?php /* Template Name: Blogg */
+get_header(); ?>
+
+<main>
+    <section>
+			<div class="container">
+				<div class="row">
+					<div id="primary" class="col-xs-12 col-md-9">
+						<h1> <?php the_category(", "); ?> </h1>
+                            <article>
+            <?php 
+            // the query
+            $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+            
+            <?php if ( $wpb_all_query->have_posts() ) : ?>
+ 
+
+ 
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+    
+    <img src = "<?php echo get_the_post_thumbnail_url(); ?>">
+        <h2 class="title" ><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <ul class="meta">
+			<li>
+			    <i class="fa fa-calendar"></i> <?php the_date(); ?>
+			</li>
+			<li>
+				<i class="fa fa-user"></i> <a href="forfattare.html"><?php the_author(); ?></a>
+			</li>
+            <li>
+				<i class="fa fa-tag">
+					<?php echo get_the_category_list(", "); ?>
+				</i> 
+			</li>
+		</ul>
+        
+        <p class="stycke"> <?php the_excerpt(); ?> </p>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+</article>
+	<?php get_template_part('templateparts/pagination'); ?>
+</div>
+    <aside id="secondary" class="col-xs-12 col-md-3">
+		<?php get_template_part('sidebar') ?>
+</aside>
+ 
+    <?php wp_reset_postdata(); ?>
+ 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?> 
